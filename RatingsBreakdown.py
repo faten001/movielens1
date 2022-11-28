8eec2a9220f6a93f699703c9c2e6bb5ca36e68c4
faten001 <80006052+faten001@users.noreply.github.com>
2022-11-28 20:07:38 +0300

    Update RatingsBreakdown.py

--- RatingsBreakdown.py
+++ RatingsBreakdown.py
@@ -13,12 +13,12 @@ class RatingsBreakdown(MRJob):
             reducer=self.reducer_count_ratings)
     ]
 # Define mapper to break up lines of data on tab, extracts the rating and return key value pair of rating and 1
-def mapper_get_ratings(self, _, line):
-  (userID, movieID, rating, timestamp) = line.split('\t')
-  yield rating, 1
+   def mapper_get_ratings(self, _, line):
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield rating, 1
 # Reduce key values, return rating and rating count.
 # Reducer function takes in eacn indvidual unique rating one through five and a list of all the one values associated with each rating and then it yields the key which is the rating (1 through 5) and the sum of all those ones which is eventually the count of each rating type.
-def reducer_count_ratings(self, key, values):
-    yield key, sum(values)
+   def reducer_count_ratings(self, key, values):
+        yield key, sum(values)
 if __name__ == '__main__':
     RatingsBreakdown.run()
